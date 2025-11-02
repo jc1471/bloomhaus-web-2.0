655543b52098719e6a0fa7575834361b86c3046a
jc1471 <jordanbcrow@gmail.com>
2025-11-02 16:36:03 +0100

    implement motion to fade-in image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navbar from "./Navbar";
 import Logo from "./Logo";
-import BandYellow from "../assets/BAND YELLOW.png"
+import BandYellow from "../assets/BAND YELLOW.png";
+import { motion } from "framer-motion";
 
 const Home = () => {
     return (
         <div classname="home-container">
             <Logo />
-            <img src={BandYellow} class="band-yellow"/>
+            <motion.img src={BandYellow} class="band-yellow"
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 10.8 }}/>
             <Navbar />
         </div>
     );
